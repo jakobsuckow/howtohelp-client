52f8670c52da0f61892a0cd0047a05c2336c5316
Jakob Suckow <jakob@martian.agency>
2020-05-02 01:07:58 +0200

    asd

--- src/modules/shared/bigmap/bigmap.js
+++ src/modules/shared/bigmap/bigmap.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react"
 import ReactMapGL from "react-map-gl"
-import dotenv from "dotenv"
-dotenv.config()
-
-
+require("dotenv").config()
 
 export default function BigMap() {
+  const token = process.env.MAP_GL_KEY
+  console.log(token)
   const [viewport, setViewport] = useState({
     latitude: 52.520008,
     longitude: 13.404954,
@@ -18,7 +17,7 @@ export default function BigMap() {
     <div>
       <ReactMapGL
         {...viewport}
-        mapboxApiAccessToken={process.env.MAP_GL_KEY}
+        mapboxApiAccessToken={`pk.eyJ1IjoiamFrb2JzdWNrb3ciLCJhIjoiY2s4M2pmeHo3MGI5bzNtbzVma2w3YTdkOCJ9.SoffMUvqxv6PTh5TYq20kA`}
         onViewportChange={(viewport) => {
           setViewport(viewport)
         }}
