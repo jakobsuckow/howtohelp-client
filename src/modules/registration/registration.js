1a30262fb0ecdadb3e1e2d11ef9139debae0d4a2
Jakob Suckow <jakob@martian.agency>
2020-10-16 20:15:38 +0200

    asd

--- src/modules/registration/registration.js
+++ src/modules/registration/registration.js
@@ -1,15 +1,16 @@
 import React from "react"
 import Grid from "@material-ui/core/Grid"
-import { useForm, FormContext } from "react-hook-form"
+import { useForm, Controller } from "react-hook-form"
 import Layout from "../../components/layout/layout"
 import { Box, Button } from "@material-ui/core"
 import FormInput from "../../components/form/formInput"
 import FormCheckbox from "../../components/form/formCheckbox"
 import DogWalkingIcon from "../../components/form/assets/dogWalkingIcon"
 import DeliveryIcon from "../../components/form/assets/deliveryIcon"
+import { CheckBox } from "@material-ui/icons"
 
 const Registration = () => {
-  const methods = useForm({
+  const { control, handleSubmit } = useForm({
     mode: "onBlur",
     reValidateMode: "onBlur",
     submitFocusError: false,
@@ -18,46 +19,45 @@ const Registration = () => {
     },
   })
 
-  const { handleSubmit } = methods
-
-
-
   return (
     <Layout>
-      <FormContext {...methods}>
-        <Box component="form" onSubmit={handleSubmit((d) => console.log(d))}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <FormInput
-                id="registration-firstname"
-                name="firstName"
-                label="Firstname"
-                placeholder="Enter your firstname"
-                required
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormCheckbox
-                icon={<DogWalkingIcon />}
-                label="Dogwalking"
-                checkedIcon={<DogWalkingIcon checked />}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormCheckbox
-                icon={<DeliveryIcon />}
-                label="Delivery"
-                checkedIcon={<DeliveryIcon checked />}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button type="submit">Submit</Button>
-            </Grid>
+      <Box component="form" onSubmit={handleSubmit((d) => console.log(d))}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Controller
+              render={(props) => (
+                <FormCheckbox
+                  icon={<DogWalkingIcon />}
+                  label="Dogwalking"
+                  checkedIcon={<DogWalkingIcon checked />}
+                  checked={props.value}
+                  onChange={(e) => console.log(e.target.checked)}
+                />
+              )}
+              name="dogwalking"
+              control={control}
+              defaultValue={false}
+            />
           </Grid>
-        </Box>
-      </FormContext>
+          <Grid item xs={12}>
+            <Controller as={<CheckBox />} />
+          </Grid>
+          <Grid item xs={12}>
+            <FormCheckbox
+              icon={<DeliveryIcon />}
+              label="Delivery"
+              checkedIcon={<DeliveryIcon checked />}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit">Submit</Button>
+          </Grid>
+        </Grid>
+      </Box>
     </Layout>
   )
 }
+
+// https://react-hook-form.com/api#useFormContext
 
 export default Registration
